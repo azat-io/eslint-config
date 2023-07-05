7a645cc3a62a9b03f437429f16b84bee90e2c768
Azat S <to@azat.io>
2023-07-06 00:06:59 +0300

    chore: update dependencies

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   },
   plugins: [
     dts({
-      outputDir: path.join(__dirname, 'dist'),
+      outDir: path.join(__dirname, 'dist'),
       root: path.join(__dirname, '..'),
       entryRoot: __dirname,
       include: [
