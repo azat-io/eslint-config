26e487280c862e1268081f050de21e0bc1cf7fa3
Azat S. <to@azat.io>
2025-01-19 00:25:23 +0300

    chore: rename n plugin to node

--- node/index.ts
+++ node/index.ts
@@ -20,7 +20,7 @@ export let node = async (config: ConfigOptions): Promise<Linter.Config> => {
     return {}
   }
 
-  let nPlugin = await interopDefault(import('eslint-plugin-n'))
+  let nodePlugin = await interopDefault(import('eslint-plugin-n'))
 
   let files = ['**/*.js', '**/*.cjs', '**/*.mjs']
 
@@ -54,88 +54,88 @@ export let node = async (config: ConfigOptions): Promise<Linter.Config> => {
     files,
 
     plugins: {
-      n: nPlugin,
+      node: nodePlugin,
     },
 
     rules: {
       /**
        * Require error handling in callbacks.
        */
-      'n/handle-callback-err': ['error', '^(err|error)$'],
+      'node/handle-callback-err': ['error', '^(err|error)$'],
       /**
        * Require correct usage of hashbang comments.
        */
-      'n/hashbang': 'error',
+      'node/hashbang': 'error',
       /**
        * Disallow deprecated APIs.
        */
-      'n/no-deprecated-api': 'error',
+      'node/no-deprecated-api': 'error',
       /**
        * Disallow the assignment to exports.
        */
-      'n/no-exports-assign': 'error',
+      'node/no-exports-assign': 'error',
       /**
        * Disallow `require()` expressions which import extraneous modules.
        */
-      'n/no-extraneous-require': 'error',
+      'node/no-extraneous-require': 'error',
       /**
        * Disallow `new` operators with calls to `require`.
        */
-      'n/no-new-require': 'error',
+      'node/no-new-require': 'error',
       /**
        * Disallow string concatenation with __dirname and __filename.
        */
-      'n/no-path-concat': 'error',
+      'node/no-path-concat': 'error',
       /**
        * Disallow `bin` files that npm ignores.
        */
-      'n/no-unpublished-bin': 'error',
+      'node/no-unpublished-bin': 'error',
       /**
        * Disallow unsupported ECMAScript built-ins on the specified version.
        */
-      'n/no-unsupported-features/es-builtins': 'error',
+      'node/no-unsupported-features/es-builtins': 'error',
       /**
        * Enforce either `Buffer` or `require("buffer").Buffer`
        */
-      'n/prefer-global/buffer': ['error', 'always'],
+      'node/prefer-global/buffer': ['error', 'always'],
       /**
        * Enforce either `console` or `require("console")`.
        */
-      'n/prefer-global/console': ['error', 'always'],
+      'node/prefer-global/console': ['error', 'always'],
       /**
        * Enforce either `process` or `require("process")`.
        */
-      'n/prefer-global/process': ['error', 'always'],
+      'node/prefer-global/process': ['error', 'always'],
       /**
        * Enforce either `TextDecoder` or `require("util").TextDecoder`.
        */
-      'n/prefer-global/text-decoder': ['error', 'always'],
+      'node/prefer-global/text-decoder': ['error', 'always'],
       /**
        * Enforce either `TextEncoder` or `require("util").TextEncoder`.
        */
-      'n/prefer-global/text-encoder': ['error', 'always'],
+      'node/prefer-global/text-encoder': ['error', 'always'],
       /**
        * Enforce either `URL` or `require("url").URL`.
        */
-      'n/prefer-global/url': ['error', 'always'],
+      'node/prefer-global/url': ['error', 'always'],
       /**
        * Enforce either `URLSearchParams` or `require("url").URLSearchParams`.
        */
-      'n/prefer-global/url-search-params': ['error', 'always'],
+      'node/prefer-global/url-search-params': ['error', 'always'],
       /**
        * Enforce using the `node:` protocol when importing Node.js builtin
        * modules.
        */
-      'n/prefer-node-protocol': 'error',
+      'node/prefer-node-protocol': 'error',
       /**
        * Enforce `require("fs").promises`.
        */
-      'n/prefer-promises/fs': 'error',
+      'node/prefer-promises/fs': 'error',
       /**
        * Require that `process.exit()` expressions use the same code path as
        * throw.
        */
-      'n/process-exit-as-throw': 'error',
+      'node/process-exit-as-throw': 'error',
     },
   }
 }
