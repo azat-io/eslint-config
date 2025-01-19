a72e1f3f6bfe459e718f11084ed40b1b31edb426
Azat S. <to@azat.io>
2025-01-19 13:48:54 +0300

    chore: rename typescript-eslint plugin to typescript

--- typescript/index.ts
+++ typescript/index.ts
@@ -62,14 +62,14 @@ export let typescript = async (
     },
 
     plugins: {
-      '@typescript-eslint': typescriptPlugin,
+      typescript: typescriptPlugin,
     },
 
     rules: {
       /**
        * Require consistently using either `T[]` or `Array<T>` for arrays.
        */
-      '@typescript-eslint/array-type': [
+      'typescript/array-type': [
         'error',
         {
           default: 'array',
@@ -79,51 +79,48 @@ export let typescript = async (
       /**
        * Disallow awaiting a value that is not a Thenable.
        */
-      '@typescript-eslint/await-thenable': 'error',
+      'typescript/await-thenable': 'error',
       /**
        * Enforce that literals on classes are exposed in a consistent style.
        */
-      '@typescript-eslint/class-literal-property-style': 'error',
+      'typescript/class-literal-property-style': 'error',
       /**
        * Enforce that class methods utilize `this` or use `static` methods.
        */
-      '@typescript-eslint/class-methods-use-this': 'error',
+      'typescript/class-methods-use-this': 'error',
       /**
        * Enforce specifying generic type arguments on constructor name of a
        * constructor call.
        */
-      '@typescript-eslint/consistent-generic-constructors': [
-        'error',
-        'constructor',
-      ],
+      'typescript/consistent-generic-constructors': ['error', 'constructor'],
       /**
        * Require the `Record` type.
        */
-      '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
+      'typescript/consistent-indexed-object-style': ['error', 'record'],
       /**
        * Enforce using `interface` for object type definitions.
        */
-      '@typescript-eslint/consistent-type-definitions': 'error',
+      'typescript/consistent-type-definitions': 'error',
       /**
        * Enforce consistent usage of type exports.
        */
-      '@typescript-eslint/consistent-type-exports': 'error',
+      'typescript/consistent-type-exports': 'error',
       /**
        * Enforce consistent usage of type imports.
        */
-      '@typescript-eslint/consistent-type-imports': 'error',
+      'typescript/consistent-type-imports': 'error',
       /**
        * Enforce default parameters to be last.
        */
-      '@typescript-eslint/default-param-last': 'error',
+      'typescript/default-param-last': 'error',
       /**
        * Enforce dot notation whenever possible.
        */
-      '@typescript-eslint/dot-notation': 'error',
+      'typescript/dot-notation': 'error',
       /**
        * Require explicit return types on functions and class methods.
        */
-      '@typescript-eslint/explicit-function-return-type': [
+      'typescript/explicit-function-return-type': [
         'error',
         {
           // https://github.com/withastro/compiler/issues/554
@@ -137,12 +134,12 @@ export let typescript = async (
        * Require explicit accessibility modifiers on class properties and
        * methods.
        */
-      '@typescript-eslint/explicit-member-accessibility': 'error',
+      'typescript/explicit-member-accessibility': 'error',
       /**
        * Require explicit return and argument types on exported functions' and
        * classes' public class methods.
        */
-      '@typescript-eslint/explicit-module-boundary-types': [
+      'typescript/explicit-module-boundary-types': [
         'error',
         {
           // https://github.com/withastro/compiler/issues/554
@@ -152,7 +149,7 @@ export let typescript = async (
       /**
        * Enforce a maximum number of parameters in function definitions.
        */
-      '@typescript-eslint/max-params': [
+      'typescript/max-params': [
         'error',
         {
           max: 3,
@@ -161,11 +158,11 @@ export let typescript = async (
       /**
        * Enforce using property signature for functions.
        */
-      '@typescript-eslint/method-signature-style': ['error', 'method'],
+      'typescript/method-signature-style': ['error', 'method'],
       /**
        * Enforce naming conventions for everything across a codebase.
        */
-      '@typescript-eslint/naming-convention': [
+      'typescript/naming-convention': [
         'error',
         {
           format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
@@ -184,40 +181,40 @@ export let typescript = async (
       /**
        * Disallow generic `Array` constructors.
        */
-      '@typescript-eslint/no-array-constructor': 'error',
+      'typescript/no-array-constructor': 'error',
       /**
        * Disallow using the `delete` operator on array values.
        */
-      '@typescript-eslint/no-array-delete': 'error',
+      'typescript/no-array-delete': 'error',
       /**
        * Require `.toString()` and `.toLocaleString()` to only be called on
        * objects which provide useful information when stringified.
        */
-      '@typescript-eslint/no-base-to-string': 'error',
+      'typescript/no-base-to-string': 'error',
       /**
        * Disallow duplicate enum member values.
        */
-      '@typescript-eslint/no-duplicate-enum-values': 'error',
+      'typescript/no-duplicate-enum-values': 'error',
       /**
        * Disallow duplicate constituents of union or intersection types.
        */
-      '@typescript-eslint/no-duplicate-type-constituents': 'error',
+      'typescript/no-duplicate-type-constituents': 'error',
       /**
        * Disallow accidentally using the "empty object" type.
        */
-      '@typescript-eslint/no-empty-object-type': 'error',
+      'typescript/no-empty-object-type': 'error',
       /**
        * Disallow the `any` type.
        */
-      '@typescript-eslint/no-explicit-any': 'error',
+      'typescript/no-explicit-any': 'error',
       /**
        * Disallow extra non-null assertions.
        */
-      '@typescript-eslint/no-extra-non-null-assertion': 'error',
+      'typescript/no-extra-non-null-assertion': 'error',
       /**
        * Require Promise-like statements to be handled appropriately.
        */
-      '@typescript-eslint/no-floating-promises': [
+      'typescript/no-floating-promises': [
         'error',
         {
           allowForKnownSafeCalls: [
@@ -235,37 +232,37 @@ export let typescript = async (
       /**
        * Disallow iterating over an array with a for-in loop.
        */
-      '@typescript-eslint/no-for-in-array': 'error',
+      'typescript/no-for-in-array': 'error',
       /**
        * Disallow the use of `eval()`-like methods.
        */
-      '@typescript-eslint/no-implied-eval': 'error',
+      'typescript/no-implied-eval': 'error',
       /**
        * Enforce the use of top-level import type qualifier when an import only
        * has specifiers with inline type qualifiers.
        */
-      '@typescript-eslint/no-import-type-side-effects': 'error',
+      'typescript/no-import-type-side-effects': 'error',
       /**
        * Disallow `void` type outside of generic or return types.
        */
-      '@typescript-eslint/no-invalid-void-type': 'error',
+      'typescript/no-invalid-void-type': 'error',
       /**
        * Disallow function declarations that contain unsafe references inside
        * loop statements.
        */
-      '@typescript-eslint/no-loop-func': 'error',
+      'typescript/no-loop-func': 'error',
       /**
        * Disallow the `void` operator except when used to discard a value.
        */
-      '@typescript-eslint/no-meaningless-void-operator': 'error',
+      'typescript/no-meaningless-void-operator': 'error',
       /**
        * Enforce valid definition of `new` and `constructor`.
        */
-      '@typescript-eslint/no-misused-new': 'error',
+      'typescript/no-misused-new': 'error',
       /**
        * Disallow Promises in places not designed to handle them.
        */
-      '@typescript-eslint/no-misused-promises': [
+      'typescript/no-misused-promises': [
         'error',
         {
           checksVoidReturn: false,
@@ -274,108 +271,107 @@ export let typescript = async (
       /**
        * Disallow enums from having both number and string members.
        */
-      '@typescript-eslint/no-mixed-enums': 'error',
+      'typescript/no-mixed-enums': 'error',
       /**
        * Disallow TypeScript namespaces.
        */
-      '@typescript-eslint/no-namespace': 'error',
+      'typescript/no-namespace': 'error',
       /**
        * Disallow non-null assertions in the left operand of a nullish
        * coalescing operator.
        */
-      '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
+      'typescript/no-non-null-asserted-nullish-coalescing': 'error',
       /**
        * Disallow non-null assertions after an optional chain expression.
        */
-      '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+      'typescript/no-non-null-asserted-optional-chain': 'error',
       /**
        * Disallow invocation of `require()`.
        */
-      '@typescript-eslint/no-require-imports': 'error',
+      'typescript/no-require-imports': 'error',
       /**
        * Disallow variable declarations from shadowing variables declared in the
        * outer scope.
        */
-      '@typescript-eslint/no-shadow': 'error',
+      'typescript/no-shadow': 'error',
       /**
        * Disallow aliasing `this`.
        */
-      '@typescript-eslint/no-this-alias': 'error',
+      'typescript/no-this-alias': 'error',
       /**
        * Disallow unnecessary equality comparisons against boolean literals.
        */
-      '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+      'typescript/no-unnecessary-boolean-literal-compare': 'error',
       /**
        * Disallow conditionals where the type is always truthy or always falsy.
        */
-      '@typescript-eslint/no-unnecessary-condition': 'error',
+      'typescript/no-unnecessary-condition': 'error',
       /**
        * Disallow unnecessary assignment of constructor property parameter.
        */
-      '@typescript-eslint/no-unnecessary-parameter-property-assignment':
-        'error',
+      'typescript/no-unnecessary-parameter-property-assignment': 'error',
       /**
        * Disallow unnecessary namespace qualifiers.
        */
-      '@typescript-eslint/no-unnecessary-qualifier': 'error',
+      'typescript/no-unnecessary-qualifier': 'error',
       /**
        * Disallow unnecessary template expressions.
        */
-      '@typescript-eslint/no-unnecessary-template-expression': 'error',
+      'typescript/no-unnecessary-template-expression': 'error',
       /**
        * Disallow type arguments that are equal to the default.
        */
-      '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+      'typescript/no-unnecessary-type-arguments': 'error',
       /**
        * Disallow type assertions that do not change the type of an expression.
        */
-      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+      'typescript/no-unnecessary-type-assertion': 'error',
       /**
        * Disallow unnecessary constraints on generic types.
        */
-      '@typescript-eslint/no-unnecessary-type-constraint': 'error',
+      'typescript/no-unnecessary-type-constraint': 'error',
       /**
        * Disallow type parameters that aren't used multiple times.
        */
-      '@typescript-eslint/no-unnecessary-type-parameters': 'error',
+      'typescript/no-unnecessary-type-parameters': 'error',
       /**
        * Disallow calling a function with a value with type `any`.
        */
-      '@typescript-eslint/no-unsafe-argument': 'error',
+      'typescript/no-unsafe-argument': 'error',
       /**
        * Disallow assigning a value with type `any` to variables and properties.
        */
-      '@typescript-eslint/no-unsafe-assignment':
+      'typescript/no-unsafe-assignment':
         // https://github.com/ota-meshi/eslint-plugin-astro/issues/341
         config.astro ? 'off' : 'error',
       /**
        * Disallow calling a value with type `any`.
        */
-      '@typescript-eslint/no-unsafe-call': 'error',
+      'typescript/no-unsafe-call': 'error',
       /**
        * Disallow unsafe declaration merging.
        */
-      '@typescript-eslint/no-unsafe-declaration-merging': 'error',
+      'typescript/no-unsafe-declaration-merging': 'error',
       /**
        * Disallow using the unsafe built-in Function type.
        */
-      '@typescript-eslint/no-unsafe-function-type': 'error',
+      'typescript/no-unsafe-function-type': 'error',
       /**
        * Disallow member access on a value with type `any`.
        */
-      '@typescript-eslint/no-unsafe-member-access': 'error',
+      'typescript/no-unsafe-member-access': 'error',
       /**
        * Disallow returning a value with type `any` from a function.
        */
-      '@typescript-eslint/no-unsafe-return': 'error',
+      'typescript/no-unsafe-return': 'error',
       /**
        * Require unary negation to take a number.
        */
-      '@typescript-eslint/no-unsafe-unary-minus': 'error',
+      'typescript/no-unsafe-unary-minus': 'error',
       /**
        * Disallow unused expressions.
        */
-      '@typescript-eslint/no-unused-expressions': [
+      'typescript/no-unused-expressions': [
         'error',
         {
           allowShortCircuit: true,
@@ -386,7 +382,7 @@ export let typescript = async (
       /**
        * Disallow unused variables.
        */
-      '@typescript-eslint/no-unused-vars': [
+      'typescript/no-unused-vars': [
         'error',
         {
           argsIgnorePattern: '^_',
@@ -398,7 +394,7 @@ export let typescript = async (
       /**
        * Disallow the use of variables before they are defined.
        */
-      '@typescript-eslint/no-use-before-define': [
+      'typescript/no-use-before-define': [
         'error',
         {
           classes: false,
@@ -409,108 +405,107 @@ export let typescript = async (
       /**
        * Disallow unnecessary constructors.
        */
-      '@typescript-eslint/no-useless-constructor': 'error',
+      'typescript/no-useless-constructor': 'error',
       /**
        * Disallow empty exports that don't change anything in a module file.
        */
-      '@typescript-eslint/no-useless-empty-export': 'error',
+      'typescript/no-useless-empty-export': 'error',
       /**
        * Disallow using confusing built-in primitive class wrappers.
        */
-      '@typescript-eslint/no-wrapper-object-types': 'error',
+      'typescript/no-wrapper-object-types': 'error',
       /**
        * Enforce non-null assertions over explicit type casts.
        */
-      '@typescript-eslint/non-nullable-type-assertion-style': 'error',
+      'typescript/non-nullable-type-assertion-style': 'error',
       /**
        * Disallow throwing non-`Error` values as exceptions.
        */
-      '@typescript-eslint/only-throw-error': 'error',
+      'typescript/only-throw-error': 'error',
       /**
        * Enforce the use of `as const` over literal type.
        */
-      '@typescript-eslint/prefer-as-const': 'error',
+      'typescript/prefer-as-const': 'error',
       /**
        * Require destructuring from arrays and/or objects.
        */
-      '@typescript-eslint/prefer-destructuring': 'error',
+      'typescript/prefer-destructuring': 'error',
       /**
        * Require each enum member value to be explicitly initialized.
        */
-      '@typescript-eslint/prefer-enum-initializers': 'error',
+      'typescript/prefer-enum-initializers': 'error',
       /**
        * Enforce the use of `for-of` loop over the standard `for` loop where
        * possible.
        */
-      '@typescript-eslint/prefer-for-of': 'error',
+      'typescript/prefer-for-of': 'error',
       /**
        * Enforce using function types instead of interfaces with call
        * signatures.
        */
-      '@typescript-eslint/prefer-function-type': 'error',
+      'typescript/prefer-function-type': 'error',
       /**
        * Enforce using the nullish coalescing operator instead of logical
        * assignments or chaining.
        */
-      '@typescript-eslint/prefer-nullish-coalescing': 'error',
+      'typescript/prefer-nullish-coalescing': 'error',
       /**
        * Enforce using concise optional chain expressions instead of chained
        * logical ands, negated logical ors, or empty objects.
        */
-      '@typescript-eslint/prefer-optional-chain': 'error',
+      'typescript/prefer-optional-chain': 'error',
       /**
        * Require using Error objects as Promise rejection reasons.
        */
-      '@typescript-eslint/prefer-promise-reject-errors': 'error',
+      'typescript/prefer-promise-reject-errors': 'error',
       /**
        * Require private members to be marked as `readonly` if they're never
        * modified outside of the constructor.
        */
-      '@typescript-eslint/prefer-readonly': 'error',
+      'typescript/prefer-readonly': 'error',
       /**
        * Enforce using type parameter when calling `Array#reduce` instead of
        * casting.
        */
-      '@typescript-eslint/prefer-reduce-type-parameter': 'error',
+      'typescript/prefer-reduce-type-parameter': 'error',
       /**
        * Enforce that `this` is used when only `this` type is returned.
        */
-      '@typescript-eslint/prefer-return-this-type': 'error',
+      'typescript/prefer-return-this-type': 'error',
       /**
        * Enforce that `get()` types should be assignable to their equivalent
        * `set()` type.
        */
-      '@typescript-eslint/related-getter-setter-pairs': 'error',
+      'typescript/related-getter-setter-pairs': 'error',
       /**
        * Require `Array#sort` and `Array#toSorted` calls to always provide a
        * `compareFunction`.
        */
-      '@typescript-eslint/require-array-sort-compare': 'error',
+      'typescript/require-array-sort-compare': 'error',
       /**
        * Disallow async functions which have no `await` expression.
        */
-      '@typescript-eslint/require-await': 'error',
+      'typescript/require-await': 'error',
       /**
        * Require both operands of addition to be the same type and be `bigint`,
        * `number`, or `string`.
        */
-      '@typescript-eslint/restrict-plus-operands': 'error',
+      'typescript/restrict-plus-operands': 'error',
       /**
        * Enforce template literal expressions to be of `string` type.
        */
-      '@typescript-eslint/restrict-template-expressions': 'error',
+      'typescript/restrict-template-expressions': 'error',
       /**
        * Disallow certain triple slash directives in favor of ES6-style import
        * declarations.
        */
-      '@typescript-eslint/triple-slash-reference': config.astro
-        ? 'off'
-        : 'error',
+      'typescript/triple-slash-reference': config.astro ? 'off' : 'error',
       /**
        * Disallow two overloads that could be unified into one with a union or
        * an optional/rest parameter.
        */
-      '@typescript-eslint/unified-signatures': 'error',
+      'typescript/unified-signatures': 'error',
+
       /**
        * The code problems checked by this ESLint rule is automatically checked
        * by the TypeScript compiler.
