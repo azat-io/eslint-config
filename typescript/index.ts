e83e760c0f3cacdfffeb5015200960493186aca6
Azat S. <to@azat.io>
2025-05-26 07:48:57 +0300

    feat(core): remove switch-exhaustiveness-check rule

--- typescript/index.ts
+++ typescript/index.ts
@@ -510,10 +510,6 @@ export let typescript = async (
        */
       'typescript/restrict-template-expressions': 'error',
       /**
-       * Require switch-case statements to be exhaustive.
-       */
-      'typescript/switch-exhaustiveness-check': 'error',
-      /**
        * Disallow certain triple slash directives in favor of ES6-style import
        * declarations.
        */
