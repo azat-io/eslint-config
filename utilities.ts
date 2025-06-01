562be3a847799c51e1fbf328aabe1bc0b1c7386f
Azat S. <to@azat.io>
2025-06-01 13:40:54 +0300

    feat: support node v24

--- utilities.ts
+++ utilities.ts
@@ -3,14 +3,8 @@ export let interopDefault = async <T>(module: Promise<T> | T): Promise<T> => {
     let resolved = await module
 
     if (typeof resolved === 'object' && resolved !== null) {
-      if ('default' in resolved && Object.keys(resolved).length === 1) {
-        let defaultExport = (resolved as { default: unknown }).default as T
-
-        if (!defaultExport) {
-          return defaultExport
-        }
-
-        return defaultExport
+      if ('default' in resolved) {
+        return (resolved as { default: unknown }).default as T
       }
       return resolved
     }
