e8912c41576120885b4a9823465ded06eedbbfa9
Azat S. <to@azat.io>
2025-06-13 17:13:30 +0300

    feat(package-json): add valid-author rule

--- package-json/index.ts
+++ package-json/index.ts
@@ -96,6 +96,10 @@ export let packageJson = (_config: ConfigOptions): Linter.Config => ({
      */
     'package-json/unique-dependencies': 'error',
     /**
+     * Validate the `author` property.
+     */
+    'package-json/valid-author': 'error',
+    /**
      * Validate the `bin` property.
      */
     'package-json/valid-bin': 'error',
