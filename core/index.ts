a4c50cc370a50ed69ae2ce9e5742154cfcca4098
Azat S. <to@azat.io>
2026-03-09 22:13:33 +0300

    fix(core): remove deprecated no-unused-disable rule

--- core/index.ts
+++ core/index.ts
@@ -839,10 +839,6 @@ export function core(config: ConfigOptions): Linter.Config {
        */
       'eslint-comments/no-unlimited-disable': 'error',
       /**
-       * Disallow unused `eslint-disable` comments.
-       */
-      'eslint-comments/no-unused-disable': 'error',
-      /**
        * Disallow unused `eslint-enable` comments.
        */
       'eslint-comments/no-unused-enable': 'error',
